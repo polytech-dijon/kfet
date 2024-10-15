fb59a361c230a67c59992961966486541ce109b0
ArnoBidet <arno.bidet479@gmail.com>
2024-10-15 15:55:51 +0200

    Fix scrollbar style

--- components/commands/articlesSelector.tsx
+++ components/commands/articlesSelector.tsx
@@ -29,14 +29,14 @@ type CommandListState = [Map<string, number>,Dispatch<SetStateAction<Map<string,
 
 const containerStyle = {
   maxHeight: "40vh",
-  "&::-webkit-scrollbar": {
+  "&::WebkitScrollbar": {
     width: "0.4em",
   },
-  "&::-webkit-scrollbar-track": {
+  "&::WebkitScrollbarTrack": {
     boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
     webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
   },
-  "&::-webkit-scrollbar-thumb": {
+  "&::WebkitScrollbarThumb": {
     backgroundColor: "rgba(0,0,0,.1)",
     outline: "1px solid slategrey",
   },
